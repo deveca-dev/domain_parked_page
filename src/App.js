3d10ed181971be7764b488ef74871aab8442a2d4
deveca-dev <kevin.carter@deveca.dev>
2024-08-26 20:17:22 +0200

    chore: Add landing page component and update styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { LandingPage } from './LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        position: "relative",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        overflow: "hidden",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "110%",  // Vergrößere die Breite, um die Vignette zu minimieren
+          height: "110%", // Vergrößere die Höhe, um die Vignette zu minimieren
+          backgroundImage: `url("https://images.unsplash.com/photo-1715464881665-e539f7f41992?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`,
+          // backgroundSize: "",
+          backgroundPosition: "center",
+          backdropFilter: 'blur(1px)',
+          backgroundColor: 'rgba(0, 0, 0, 0.4)',
+          zIndex: -1,
+        }}
+      ></div>
+
+      <div
+        style={{
+          position: "relative",
+          zIndex: 1,
+          color: "white",
+          textAlign: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.5)", // Optional: Dunkelt den Textbereich leicht ab
+          padding: "50px",
+          borderRadius: "15px",
+        }}
+      >
+        <LandingPage />
+      </div>
     </div>
   );
 }
---
+++ src/LandingPage.jsx
@@ -0,0 +1,18 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+
+
+export function LandingPage() {
+  return (
+    <div
+    style={{
+       
+    }}
+    >
+      <Container>
+        <h1>Content is coming soon</h1>
+      </Container>
+    </div>
+  );
+}
